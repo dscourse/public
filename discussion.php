73ffcb3ccff4fbd56170069ff6517fe6cc48a3eb
Caner Uguz <caneruguz@gmail.com>
2013-02-11 16:28:05 -0500

    Minor changes to admin edit user and discussion.php

--- discussion.php
+++ discussion.php
@@ -13,7 +13,7 @@ ini_set('display_errors',1);
 	$discId;
 	$uId;
 	if(array_key_exists("HTTP_ORIGIN", $_SERVER)){
-		$origin = $_SERVER['HTTP_ORIGIN'];
+		$origin = $_SERVER['HTTP_ORIGIN'];		
 		$LTI_allowed = array('https://collab.itc.virginia.edu'=>'UVa Collab');
   		if(array_key_exists($origin, $LTI_allowed)){
   			$LTI = TRUE;
