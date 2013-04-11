ca1333cbddbef414a3164cbc07817a697852c29b
shc7pw <root@shc.(none)>
2013-04-11 09:59:08 -0400

    Viewer experience implemented in course.php

--- course.php
+++ course.php
@@ -226,7 +226,7 @@ ini_set('display_errors',1);
                 <div class="span8">
                     <div id="courseDiscussions">
                         <h3>Course Discussions
-                        <?php if($currentRole == 'Instructor' || $currentRole == 'TA' || $preProcess['options']['studentCreateDisc']=="Yes"){ ?>
+                        <?php if(!$currentRole == "Viewer" && ($currentRole == 'Instructor' || $currentRole == 'TA' || $preProcess['options']['studentCreateDisc']=="Yes")){ ?>
 
                          <a href="adddisc.php?c=<?php echo $courseInfo['courseID']?>" id="addDiscussionView" class="btn btn-small"> Add Discussion</a>
                          <?php }?>
